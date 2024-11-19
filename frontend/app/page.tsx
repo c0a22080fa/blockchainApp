a2a0dff37a81ad712ed686a305ddf39da5fdc625
shimada masaki <c0a22080f@edu.teu.ac.jp>
2024-11-19 10:53:01 +0900

    ittann

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,95 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { ethers } from "ethers";
+import { useEffect, useState } from 'react';
+import artifact from "../abi/MyToken.sol/MyToken.json";
+
+const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [windowEthereum, setWindowEthereum] = useState();
+
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    const { ethereum } = window as any;
+    setWindowEthereum(ethereum);
+  }, []);
+
+  const handleButtonClick = async () => {
+    if (windowEthereum) {
+      const provider = new ethers.BrowserProvider(windowEthereum);
+      const signer = await provider.getSigner();
+
+      const contract = new ethers.Contract(
+      contractAddress,
+      artifact.abi,
+      provider
+      );
+
+      const walletAddress : string = await signer.getAddress();
+      const balance = await contract.balanceOf(walletAddress);
+      setInputValue(balance.toString());
+    }
+  };
+
+  return (
+    <div>
+      <h1>Blockchain sample app</h1>
+      <button onClick={handleButtonClick}>Token owned</button>
+      <input type="text" value={inputValue} readOnly />
     </div>
   );
 }
